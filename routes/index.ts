39244c3dab6e7ab8eac82b5f41aa3ffcd8506b62
VictorLG21 <victorgaspar1@hotmail.com>
2021-06-01 10:07:08 -0300

    rotas index

---
+++ routes/index.ts
@@ -0,0 +1,32 @@
+import app = require("teem");
+import Pergunta = require("../models/Pergunta");
+
+class IndexRoute {
+  public async index(req: app.Request, res: app.Response) {
+    res.render("index.ejs", {
+      layout: "layout",
+    });
+  }
+  
+  @app.route.methodName( app.root + "/fase")
+  public async fase(req: app.Request, res: app.Response) {
+    res.render("fase.ejs", {
+      layout: "layout",
+    });
+  }
+
+  @app.route.methodName( app.root + "/pergunta")
+	public async dadosBanca(req: app.Request, res: app.Response) {
+		
+		
+			res.render("pergunta.ejs", {
+				layout: "layout",
+	
+			});
+			return;
+		}
+
+
+}
+
+export = IndexRoute;
