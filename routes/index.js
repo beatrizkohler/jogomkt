884af750aa6e5adfe38c6965e3bc579d2dca3558
VictorLG21 <victorgaspar1@hotmail.com>
2021-06-06 22:54:28 -0300

    criacao pras perguntas, rotas e colocando as alternativas certas

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,147 @@ class IndexRoute {
             layout: "layout",
         });
     }
+    async venceu(req, res) {
+        res.render("venceu.ejs", {
+            layout: "layout",
+        });
+    }
+    async gameOver(req, res) {
+        res.render("gameover.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase2(req, res) {
+        res.render("fase2.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase3(req, res) {
+        res.render("fase3.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase4(req, res) {
+        res.render("fase4.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase5(req, res) {
+        res.render("fase5.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase6(req, res) {
+        res.render("fase6.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase7(req, res) {
+        res.render("fase7.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase8(req, res) {
+        res.render("fase8.ejs", {
+            layout: "layout",
+        });
+    }
+    async fase9(req, res) {
+        res.render("fase9.ejs", {
+            layout: "layout",
+        });
+    }
     async criarPergunta(req, res) {
         res.render("criar-pergunta.ejs", {
             layout: "layout",
         });
     }
-    async dadosBanca(req, res) {
+    async pergunta2(req, res) {
+        res.render("pergunta2.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta3(req, res) {
+        res.render("pergunta3.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta4(req, res) {
+        res.render("pergunta4.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta5(req, res) {
+        res.render("pergunta5.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta6(req, res) {
+        res.render("pergunta6.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta7(req, res) {
+        res.render("pergunta7.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta8(req, res) {
+        res.render("pergunta8.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta9(req, res) {
+        res.render("pergunta9.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta10(req, res) {
+        res.render("pergunta10.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta11(req, res) {
+        res.render("pergunta11.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta12(req, res) {
+        res.render("pergunta12.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta13(req, res) {
+        res.render("pergunta13.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta14(req, res) {
+        res.render("pergunta14.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta15(req, res) {
+        res.render("pergunta2.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta16(req, res) {
+        res.render("pergunta2.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta17(req, res) {
+        res.render("pergunta17.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta18(req, res) {
+        res.render("pergunta18.ejs", {
+            layout: "layout",
+        });
+    }
+    async pergunta(req, res) {
         res.render("pergunta.ejs", {
             layout: "layout",
         });
@@ -33,10 +168,91 @@ __decorate([
     app.route.methodName(app.root + "/fase")
 ], IndexRoute.prototype, "fase", null);
 __decorate([
+    app.route.methodName(app.root + "/venceu")
+], IndexRoute.prototype, "venceu", null);
+__decorate([
+    app.route.methodName(app.root + "/gameover")
+], IndexRoute.prototype, "gameOver", null);
+__decorate([
+    app.route.methodName(app.root + "/fase2")
+], IndexRoute.prototype, "fase2", null);
+__decorate([
+    app.route.methodName(app.root + "/fase3")
+], IndexRoute.prototype, "fase3", null);
+__decorate([
+    app.route.methodName(app.root + "/fase4")
+], IndexRoute.prototype, "fase4", null);
+__decorate([
+    app.route.methodName(app.root + "/fase5")
+], IndexRoute.prototype, "fase5", null);
+__decorate([
+    app.route.methodName(app.root + "/fase6")
+], IndexRoute.prototype, "fase6", null);
+__decorate([
+    app.route.methodName(app.root + "/fase7")
+], IndexRoute.prototype, "fase7", null);
+__decorate([
+    app.route.methodName(app.root + "/fase8")
+], IndexRoute.prototype, "fase8", null);
+__decorate([
+    app.route.methodName(app.root + "/fase9")
+], IndexRoute.prototype, "fase9", null);
+__decorate([
     app.route.methodName(app.root + "/criar-pergunta")
 ], IndexRoute.prototype, "criarPergunta", null);
 __decorate([
+    app.route.methodName(app.root + "/pergunta2")
+], IndexRoute.prototype, "pergunta2", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta3")
+], IndexRoute.prototype, "pergunta3", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta4")
+], IndexRoute.prototype, "pergunta4", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta5")
+], IndexRoute.prototype, "pergunta5", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta6")
+], IndexRoute.prototype, "pergunta6", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta7")
+], IndexRoute.prototype, "pergunta7", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta8")
+], IndexRoute.prototype, "pergunta8", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta9")
+], IndexRoute.prototype, "pergunta9", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta10")
+], IndexRoute.prototype, "pergunta10", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta11")
+], IndexRoute.prototype, "pergunta11", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta12")
+], IndexRoute.prototype, "pergunta12", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta13")
+], IndexRoute.prototype, "pergunta13", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta14")
+], IndexRoute.prototype, "pergunta14", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta15")
+], IndexRoute.prototype, "pergunta15", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta16")
+], IndexRoute.prototype, "pergunta16", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta17")
+], IndexRoute.prototype, "pergunta17", null);
+__decorate([
+    app.route.methodName(app.root + "/pergunta18")
+], IndexRoute.prototype, "pergunta18", null);
+__decorate([
     app.route.methodName(app.root + "/pergunta")
-], IndexRoute.prototype, "dadosBanca", null);
+], IndexRoute.prototype, "pergunta", null);
 module.exports = IndexRoute;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -14,15 +14,177 @@ class IndexRoute {
       layout: "layout",
     });
   }
+  @app.route.methodName( app.root + "/venceu")
+  public async venceu(req: app.Request, res: app.Response) {
+    res.render("venceu.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/gameover")
+  public async gameOver(req: app.Request, res: app.Response) {
+    res.render("gameover.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase2")
+  public async fase2(req: app.Request, res: app.Response) {
+    res.render("fase2.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase3")
+  public async fase3(req: app.Request, res: app.Response) {
+    res.render("fase3.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase4")
+  public async fase4(req: app.Request, res: app.Response) {
+    res.render("fase4.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase5")
+  public async fase5(req: app.Request, res: app.Response) {
+    res.render("fase5.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase6")
+  public async fase6(req: app.Request, res: app.Response) {
+    res.render("fase6.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase7")
+  public async fase7(req: app.Request, res: app.Response) {
+    res.render("fase7.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase8")
+  public async fase8(req: app.Request, res: app.Response) {
+    res.render("fase8.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/fase9")
+  public async fase9(req: app.Request, res: app.Response) {
+    res.render("fase9.ejs", {
+      layout: "layout",
+    });
+  }
   @app.route.methodName( app.root + "/criar-pergunta")
   public async criarPergunta(req: app.Request, res: app.Response) {
     res.render("criar-pergunta.ejs", {
       layout: "layout",
     });
   }
+  @app.route.methodName( app.root + "/pergunta2")
+  public async pergunta2(req: app.Request, res: app.Response) {
+    res.render("pergunta2.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta3")
+  public async pergunta3(req: app.Request, res: app.Response) {
+    res.render("pergunta3.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta4")
+  public async pergunta4(req: app.Request, res: app.Response) {
+    res.render("pergunta4.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta5")
+  public async pergunta5(req: app.Request, res: app.Response) {
+    res.render("pergunta5.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta6")
+  public async pergunta6(req: app.Request, res: app.Response) {
+    res.render("pergunta6.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta7")
+  public async pergunta7(req: app.Request, res: app.Response) {
+    res.render("pergunta7.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta8")
+  public async pergunta8(req: app.Request, res: app.Response) {
+    res.render("pergunta8.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta9")
+  public async pergunta9(req: app.Request, res: app.Response) {
+    res.render("pergunta9.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta10")
+  public async pergunta10(req: app.Request, res: app.Response) {
+    res.render("pergunta10.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta11")
+  public async pergunta11(req: app.Request, res: app.Response) {
+    res.render("pergunta11.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta12")
+  public async pergunta12(req: app.Request, res: app.Response) {
+    res.render("pergunta12.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta13")
+  public async pergunta13(req: app.Request, res: app.Response) {
+    res.render("pergunta13.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta14")
+  public async pergunta14(req: app.Request, res: app.Response) {
+    res.render("pergunta14.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta15")
+  public async pergunta15(req: app.Request, res: app.Response) {
+    res.render("pergunta2.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta16")
+  public async pergunta16(req: app.Request, res: app.Response) {
+    res.render("pergunta2.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta17")
+  public async pergunta17(req: app.Request, res: app.Response) {
+    res.render("pergunta17.ejs", {
+      layout: "layout",
+    });
+  }
+  @app.route.methodName( app.root + "/pergunta18")
+  public async pergunta18(req: app.Request, res: app.Response) {
+    res.render("pergunta18.ejs", {
+      layout: "layout",
+    });
+  }
 
   @app.route.methodName( app.root + "/pergunta")
-	public async dadosBanca(req: app.Request, res: app.Response) {
+	public async pergunta(req: app.Request, res: app.Response) {
 		
 		
 			res.render("pergunta.ejs", {
